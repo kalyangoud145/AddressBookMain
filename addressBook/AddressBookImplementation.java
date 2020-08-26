491a67f60bb98bf0ffdee755d52f9f560b1d66f9
kalyangoud145 <kalyangoud145@gmail.com>
2020-08-26 11:16:53 +0530

    Method for Sorting based on names of persons in addressBook

--- addressBook/AddressBookImplementation.java
+++ addressBook/AddressBookImplementation.java
@@ -158,4 +158,32 @@ public class AddressBookImplementation implements AddressBookInterface{
 		}
 	}
 
+	/*Sorting of list respect to First Name*/
+	public void sortByName() {
+		Collections.sort(list, new SortByName());
+		for (Person person : list) {
+			System.out.println(person.toString());
+		}	
+	}
+
+	public class SortByName implements Comparator<Person>{
+	/* 
+	 * Compares the data and returns 0 if the  data is same 
+	 * and 1 if first data is greater
+	 * and -1 if second data is greater
+	 */
+		public int compare(Person personOne, Person personTwo) {
+			if(personOne.getFirstName().equals(personTwo.getFirstName()))
+				return 0;
+			else {
+				if(personOne.getFirstName().compareTo(personTwo.getFirstName())>0) {
+					return 1;
+					}
+				else {
+					return -1;
+					}
+				}
+			}
+	}
+
 }
